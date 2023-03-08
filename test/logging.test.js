b3cf5b38df5861fc93f240a1f0dff41e14428e12
Wietse Venema <356014+wietsevenema@users.noreply.github.com>
2023-03-08 17:40:22 +0100

    Add minimal test for logging

--- test/logging.test.js
+++ test/logging.test.js
@@ -7,9 +7,16 @@ beforeAll(async () => {
 });
 
 
-test('expect a log "Hello World" from a Cloud Run container', async () => {    
-    const helloLogs = logs.filter( l => l.textPayload.includes("Hello World!"))
+test('expect a log "Hello World" from a Cloud Run container', async () => {
+    const helloLogs = logs.map(
+        ({ textPayload, jsonPayload, severity }) => {
+            return {
+                message: textPayload || jsonPayload?.message?.message,
+                severity
+            }
+        }).filter(({message}) => message?.includes('Hello World'))
+
     expect(helloLogs.length).toBeGreaterThanOrEqual(1)
 })
 
-test.skip('expect a log "Hello World" with different severity levels', async () => {})
+test.skip('expect a log "Hello World" with different severity levels', async () => { })
